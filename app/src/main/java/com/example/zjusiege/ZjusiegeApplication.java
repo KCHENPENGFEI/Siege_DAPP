75d763f9e7837da7041237185f9cd6d7b74406d7
KCHENPENGFEI <chen_gs@zju.edu.cn>
2019-06-13 22:48:57 +0800

    first commit

---
+++ app/src/main/java/com/example/zjusiege/ZjusiegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zjusiege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjusiegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZjusiegeApplication.class, args);
+	}
+
+}
