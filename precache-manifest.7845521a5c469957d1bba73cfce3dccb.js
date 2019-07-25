991570406631b9a7513ea4291729902266b0981d
zjufishboy <1647075274@qq.com>
2019-07-26 00:26:04 +0800

    Updates

---
+++ precache-manifest.7845521a5c469957d1bba73cfce3dccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffac9047b6330b3aac8bdbbcb192ea81",
+    "url": "/Siege_DAPP/index.html"
+  },
+  {
+    "revision": "89e3d18803840d665d0e",
+    "url": "/Siege_DAPP/static/css/main.a24077a4.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/Siege_DAPP/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "89e3d18803840d665d0e",
+    "url": "/Siege_DAPP/static/js/main.64bd7bb3.chunk.js"
+  },
+  {
+    "revision": "ee75dc4a7b4a12b1f57c",
+    "url": "/Siege_DAPP/static/js/runtime~main.9a53ec65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Siege_DAPP/static/media/logo.5d5d9eef.svg"
+  }
+]);
